9064fa8e0d8515c7355a53ef116e6fd76b65e9b4
Sophia Skoglund <116593556+sophiakskoglund@users.noreply.github.com>
2024-07-17 23:57:26 -0500

    Revise script
    minor edits to the comments and a function

--- scripts/validation_statistics.m
+++ scripts/validation_statistics.m
@@ -4,16 +4,22 @@
 %             in situ Dates
 % 
 %  Author         :  Sophia K. Skoglund
-%  Last Revision  :  09/12/2021, minor comment updates 10/18/2022
-%  Language       :  MATLAB R2017a (9.2.0.556344)
+%  Last Revision  :  07/14/2024
+%  Language       :  MATLAB R2023b Update 7 (23.2.0.2515942)
+%                    + Statistics and Machine Learning Toolbox
 %   
-%  Input   :  CSMicein_DOY.mat   &   insitu_icein_DOY.mat
+%  Input   :  CSM_icein_DOY.mat   &   insitu_icein_DOY.mat
+%             16 (yrs 2002/03 - 17/18) x 13 (lakes)
 %                               OR
-%             CSMiceout_DOY.mat  &   insitu_iceout_DOY.mat
+%             CSM_iceout_DOY.mat  &   insitu_iceout_DOY.mat
+%             16 (yrs 2002/03 - 17/18) x 58 (lakes)
+%             
+%             CSM_iceout_DOY_validation_20.mat  &   insitu_iceout_DOY_validation_20.mat
+%             16 (yrs 2002/03 - 17/18) x 20 (lakes)
 %
-%             arrays are 16 (yrs 2002/03 - 17/18) x 13 (lakes) for ice in
-%                    and 16 (yrs 2002/03 - 17/18) x 58 (lakes) for ice out
-%
+%             CSM_iceout_DOY_validation_58.mat  &   insitu_iceout_DOY_validation_58.mat
+%             5 (yrs 2018/19 - 22/23) x 58 (lakes)
+%             
 %  Output  :  bias, mean average error, statistics
 %             % of model approximations within _ days of error (array)
 %             
@@ -39,8 +45,8 @@
 IS = insitu_icein_DOY; % IS = in situ
 RS = CSM_icein_DOY; % RS = Remote Sensing (CSM-approximated ice in/out)
 
-seasoncount = 16; % # of rows in above variables
-lakecount = 13; % # of columns in above variables
+seasoncount = 5; % # of rows in above variables
+lakecount = 58; % # of columns in above variables
 
 % reshape to single columns
 IScolumn = reshape(IS,seasoncount*lakecount,1);
@@ -55,7 +61,7 @@ error = RScolumn-IScolumn;
 % - value means RS is x days early, + value means RS is x days late
 
 % calculate overall bias of the approximation model
-bias = nanmean(error);
+bias = mean(error,"omitnan");
 
 %%%%%%%%%
 %% III %%
@@ -65,7 +71,7 @@ bias = nanmean(error);
 abserror = abs(error);
 
 % calculate overall Mean Absolute Error (MAE)
-MAE = nanmean(nanmean(abserror));
+MAE = mean(mean(abserror,"omitnan"),"omitnan");
 
 %%%%%%%%
 %% IV %%
